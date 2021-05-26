cc6500bca21d49c69b2824e29ae09625b4bba694
Brent Buchholz <entomdt@gmail.com>
2021-05-26 12:16:42 +0000

    Add Router routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <div className="container">
+            <Navbar />
+            <br/>
+            <Route path="/" exact component={ExerciseList} />
+            <Route path="/edit/:id" component={ExerciseEdit} />
+            <Route path="/create" component={ExerciseCreate} />
+            <Route path="/user" component={UserCreate} />
+            
+        </div>
+    </Router>
   );
 }
 
