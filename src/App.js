f58331586b18f8bc4afe9df92aba31d5a74b8177
Brent Buchholz <entomdt@gmail.com>
2021-05-27 12:26:30 +0000

    Add stub components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
+import ExerciseList from './ExerciseList.component';
+import ExerciseEdit from './ExerciseEdit.component';
+import ExerciseCreate from './ExerciseCreate.component';
+import UserCreate from './UserCreate.component';
+import NavBar from './NavBar.component';
 
 
 function App() {
   return (
     <Router>
         <div className="container">
-            <Navbar />
+            <NavBar />
             <br/>
             <Route path="/" exact component={ExerciseList} />
             <Route path="/edit/:id" component={ExerciseEdit} />
